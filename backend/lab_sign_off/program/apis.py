e0de4c6bdec9a1575d25d6a5e7864828cede0bf7
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-06-17 22:53:48 +0100

    Feature-Backend: Added api for program list and update views

--- backend/lab_sign_off/program/apis.py
+++ backend/lab_sign_off/program/apis.py
@@ -14,8 +14,42 @@ from rest_framework.authentication import SessionAuthentication,TokenAuthenticat
 
 class CreateProgramView(CreateAPIView):
     """
-    Api view to create the book details
+    Api view to create a new program
     """
     authentication_classes = [SessionAuthentication, TokenAuthentication]
     permission_classes = [IsAdminOrStaffUser]
     serializer_class = ProgramsSerializer
+    
+class ProgramsListView(ListAPIView):
+    """
+    Api view to list all the programs
+    """
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAdminOrStaffUser]
+    serializer_class = ProgramsSerializer
+
+    def get_queryset(self):
+        queryset = Programs.objects.all()
+        return queryset
+
+class ProgramListView(RetrieveAPIView):
+    """
+    Api view to show a single the program
+    """
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAdminOrStaffUser]
+    serializer_class = ProgramsSerializer
+    def get_queryset(self):
+        queryset = Programs.objects.all()
+        return queryset
+
+class UpdateProgramDetailsView(UpdateAPIView):
+    """
+    Api view to update the program details
+    """
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAdminOrStaffUser]
+    serializer_class = ProgramsSerializer
+    def get_queryset(self):
+        queryset = Programs.objects.all()
+        return queryset
