74da92de148004e82e82b8c7b476c5d413a0d084
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-06-17 10:14:14 +0100

    Backend: Made Changes to the User file and made new migrations

--- backend/lab_sign_off/user/models.py
+++ backend/lab_sign_off/user/models.py
@@ -7,16 +7,21 @@ class User(AbstractUser):
     Model for Custom User
     """
     STATUS = (
-        ("staff","staff"), 
-        ("student","student")
+        ("staff","Staff"), 
+        ("student","Student")
     )
 
-    email = models.EmailField(unique = True)
-    status = models.CharField(max_length = 100, choices = STATUS,default="student")
+    email = models.EmailField(unique = True, error_messages=({'unique':"A user with that email already exists."}))
+    status = models.CharField(max_length = 10, choices = STATUS,default="student")
     updated_on = models.DateTimeField(auto_now=True)
-    otp = models.IntegerField(default = 000000)
+    otp = models.PositiveIntegerField(default = 000000)
     email_verified = models.BooleanField(default = False)
 
     def __str__(self):
         return self.email
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['email']
     
