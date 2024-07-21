5b0e9e8ca988ddbe8afb4c10e07a3cfbcd5d8a82
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-07-21 15:39:02 +0100

    Frontend: Added data to the details page

--- frontend/lab_sign_off_fe/src/component/Staff/CoursesLabsDetailPage.jsx
+++ frontend/lab_sign_off_fe/src/component/Staff/CoursesLabsDetailPage.jsx
@@ -1,9 +1,71 @@
+import { Grid } from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axiosInstance from "../../utils/Axios";
+import { tokenLoader } from "../../utils/token";
+import { Bounce, toast } from "react-toastify";
+
+const token = tokenLoader();
 
 const CoursesLabsDetailPage = () => {
   const { selectedRowId } = useParams();
+  const [courseDetails, setCourseDetails] = useState({});
 
-  return <div>Course Details for ID: {selectedRowId}</div>;
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axiosInstance.get(`/courses/${selectedRowId}/`, {
+        headers: {
+          Authorization: "Token " + token,
+        },
+      });
+      setCourseDetails(response.data); // Assign response data to courseDetails
+    } catch (error) {
+      let errorMessage = "Error loading data";
+      const errorData = error.response;
+      if (
+        errorData?.data.detail ===
+        "You do not have permission to perform this action."
+      ) {
+        errorMessage = "You do not have permission to perform this action.";
+      }
+      toast.error(errorMessage, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+  }, [selectedRowId]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  console.log(courseDetails);
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <h1>Course Details for ID: {selectedRowId}</h1>
+      </Grid>
+      <Grid item xs={12}>
+        {courseDetails.id && <p>Course Description: {courseDetails.id}</p>}
+      </Grid>
+      <Grid item xs={12}>
+        {courseDetails.course_name && (
+          <p>Course Name: {courseDetails.course_name}</p>
+        )}
+      </Grid>
+      <Grid item xs={12}>
+        {/* Add more course details here */}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CoursesLabsDetailPage;
