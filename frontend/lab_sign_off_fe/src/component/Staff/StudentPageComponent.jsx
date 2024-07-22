7e8a7d21623b904e91ca604f2c66b39cb25b586a
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-07-22 20:13:02 +0100

    Frontend :Did half of the enrollment page

--- frontend/lab_sign_off_fe/src/component/Staff/StudentPageComponent.jsx
+++ frontend/lab_sign_off_fe/src/component/Staff/StudentPageComponent.jsx
@@ -1,10 +1,26 @@
-import { Container } from "@mui/material";
+import { Container, Tab, Tabs } from "@mui/material";
 import StudentsListMainGridComponent from "./Students/StudentsListMainGridComponent";
+import { useState } from "react";
 
 const StudentPageComponent = () => {
+  const [tabItem, setTabItem] = useState("enroll");
+  const handleChange = (event, newValue) => {
+    setTabItem(newValue);
+  };
   return (
     <Container>
-      <StudentsListMainGridComponent />
+      <Tabs
+        value={tabItem}
+        onChange={handleChange}
+        textColor="primary"
+        indicatorColor="primary"
+        aria-label="secondary tabs example"
+      >
+        <Tab value="enroll" label="Enroll Student" />
+        <Tab value="mark" label="Student Status" />
+      </Tabs>
+      {tabItem === "enroll" && <StudentsListMainGridComponent />}
+      {tabItem === "mark" && <b>Test</b>}
     </Container>
   );
 };
--- frontend/lab_sign_off_fe/src/component/Staff/Students/StudentDetailPage.jsx
+++ frontend/lab_sign_off_fe/src/component/Staff/Students/StudentDetailPage.jsx
@@ -1,8 +1,101 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { tokenLoader } from "../../../utils/token";
+import axiosInstance from "../../../utils/Axios";
+import { Bounce, toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  Container,
+  Typography,
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListItemText,
+  CircularProgress,
+} from "@mui/material";
+import Button from "react-bootstrap/esm/Button";
+
+const token = tokenLoader();
 
 const StudentDetailPage = () => {
+  const [data, setData] = useState(null);
   const { selectedRowId } = useParams();
-  return <div>Student Detail Page with {selectedRowId}</div>;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get(
+          `lab-session/student-enrollment/${selectedRowId}/`,
+          {
+            headers: {
+              Authorization: "Token " + token,
+            },
+          }
+        );
+        setData(response.data);
+      } catch (error) {
+        let errorMessage = "Error loading data";
+        const errorData = error.response;
+        if (
+          errorData?.data.detail ===
+          "You do not have permission to perform this action."
+        ) {
+          errorMessage = "You do not have permission to perform this action.";
+        }
+        toast.error(errorMessage, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+      }
+    };
+    fetchData();
+  }, [selectedRowId]);
+
+  if (!data) {
+    return (
+      <Container style={{ textAlign: "center", marginTop: "50px" }}>
+        <CircularProgress />
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <ToastContainer />
+      <Typography variant="h4" gutterBottom>
+        Student Details
+      </Typography>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {data.first_name} {data.last_name}
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            <strong>Email:</strong> {data.email}
+          </Typography>
+          <Typography variant="h6" gutterBottom style={{ marginTop: "16px" }}>
+            Enrolled Courses:
+          </Typography>
+          <List>
+            {data.enrolled_courses.map((course) => (
+              <ListItem key={course.id}>
+                <ListItemText primary={course.course_name} />
+              </ListItem>
+            ))}
+          </List>
+        </CardContent>
+      </Card>
+      <Button variant="dark">Enroll</Button>
+    </Container>
+  );
 };
 
 export default StudentDetailPage;
