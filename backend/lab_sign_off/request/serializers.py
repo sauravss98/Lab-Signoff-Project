5987ff1b31d00bf0c228841bc78fa59fd0b9bc5a
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-09-04 15:39:43 +0100

    Backend: Documentation - Created docs for serializer for request app

--- backend/lab_sign_off/request/serializers.py
+++ backend/lab_sign_off/request/serializers.py
@@ -5,6 +5,14 @@ from labsession.serializers import StudentLabSessionSerializer
 from user.models import User
 
 class UserDetailsSerializer(serializers.ModelSerializer):
+    """ Serializer for user details
+
+    Args:
+        serializers (_type_): ModelSerializer
+
+    Returns:
+        _type_: Serialized data
+    """
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'user_type']
@@ -15,6 +23,11 @@ class UserDetailsSerializer(serializers.ModelSerializer):
         return super().to_representation(instance)
 
 class RequestMessageSerializer(serializers.ModelSerializer):
+    """ Serializer for request message
+
+    Args:
+        serializers (_type_): ModelSerializer
+    """
     sender = UserDetailsSerializer(read_only=True)
 
     class Meta:
@@ -22,6 +35,11 @@ class RequestMessageSerializer(serializers.ModelSerializer):
         fields = ['id', 'sender', 'message', 'file', 'created_at']
 
 class LabRequestSerializer(serializers.ModelSerializer):
+    """ Serializer for lab request
+
+    Args:
+        serializers (_type_): ModelSerializer
+    """
     student = UserDetailsSerializer(read_only=True)
     staff = UserDetailsSerializer(many=True, read_only=True)
     student_lab_session = StudentLabSessionSerializer(read_only=True)
@@ -32,11 +50,21 @@ class LabRequestSerializer(serializers.ModelSerializer):
         fields = ['id', 'student_lab_session', 'student', 'staff', 'text', 'file', 'status', 'created_at', 'updated_at', 'messages']
 
 class CreateLabRequestSerializer(serializers.ModelSerializer):
+    """ Serializer for lab request create api
+
+    Args:
+        serializers (_type_): ModelSerializer
+    """
     class Meta:
         model = LabRequest
         fields = ['student_lab_session', 'text', 'file']
 
 class CreateRequestMessageSerializer(serializers.ModelSerializer):
+    """ Serializer for message in request
+
+    Args:
+        serializers (_type_): ModelSerializer
+    """
     class Meta:
         model = RequestMessage
         fields = ['message', 'file']
