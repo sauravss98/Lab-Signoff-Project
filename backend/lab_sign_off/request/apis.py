712d1f76f4a4619cd576498830c8ae4157abc46a
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-09-05 16:19:04 +0100

    Backend: Documentation- Created docs for an api

--- backend/lab_sign_off/request/apis.py
+++ backend/lab_sign_off/request/apis.py
@@ -89,6 +89,14 @@ class IsStudentOrStaff(BasePermission):
 # Cached
 @method_decorator(cache_page(60 * 15), name='dispatch')  # Cache for 15 minutes
 class ListLabRequestsView(generics.ListAPIView):
+    """Class for request list api view. Cached api
+
+    Args:
+        generics (_type_): ListAPIView
+
+    Returns:
+        _type_: queryset
+    """
     serializer_class = LabRequestSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [SessionAuthentication, TokenAuthentication]
